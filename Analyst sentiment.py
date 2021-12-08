2a11843b2e344c4c331bd559b44da567c587107f
RS1987X <richard.kj.steffen@gmail.com>
2021-12-08 17:35:31 +0100

    data update

--- Analyst sentiment.py
+++ Analyst sentiment.py
@@ -7,10 +7,10 @@ Created on Mon Nov 22 13:05:30 2021
 
 import pandas as pd
 xl = pd.read_excel('Analyst rating changes.xlsx')
-
-
-n_upgrades = pd.DataFrame(columns=['Name','Upgrades'])
-n_upgrades.set_axis(['Name', 'Upgrades'],axis=1)
-for x in xl:
-    if x["Rating changes (steps)"] == 1 && x["Rating"] == "Buy":
-        n_upgrades = 
+#
+#
+#n_upgrades = pd.DataFrame(columns=['Name','Upgrades'])
+#n_upgrades.set_axis(['Name', 'Upgrades'],axis=1)
+#for x in xl:
+#    if x["Rating changes (steps)"] == 1 && x["Rating"] == "Buy":
+#        n_upgrades = 
